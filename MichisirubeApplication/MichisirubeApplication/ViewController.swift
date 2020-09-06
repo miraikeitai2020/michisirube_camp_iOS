9e999fc7feed500fff25d90346188680d07da0f1
AnboYukari <b1018129@gmail.com>
2020-09-06 18:52:02 +0900

    .gitignoreを編集した

---
+++ MichisirubeApplication/MichisirubeApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MichisirubeApplication
+//
+//  Created by 安保友香梨 on 2020/09/06.
+//  Copyright © 2020 anbo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
